233aded315bc76d5e706fbeb6a5bbb2e38e0287d
logalakshmi <logalakshmi.m2025aids@sece.ac.in>
2026-09-17 08:52:54 +0530

    SpringBoot

---
+++ springbootapp1/src/main/java/com/logalakshmi1/springbootapp1/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.logalakshmi1.springbootapp1.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "Manganx application is running";
+	}
+	@PostMapping("/create")
+    public String create(@RequestBody String payload) {
+		return payload + " successfully created";
+    }
+	@DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable String id) {
+		return id + " deleted successfully";
+    }
+}
